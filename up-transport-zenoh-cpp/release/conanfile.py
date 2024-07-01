6789f812260a375a11701202ff91c9437967c0a9
hzkv71 <agoshpreet.1.singh@gm.com>
2024-07-01 14:32:02 -0400

    Adding release conan recipes

---
+++ up-transport-zenoh-cpp/release/conanfile.py
@@ -0,0 +1,73 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.tools.scm import Git
+from conan.tools.files import copy, get
+import os
+import yaml
+
+class upZenohTransportRecipe(ConanFile):
+    name = "up-transport-zenoh-cpp"
+
+    # Optional metadata
+    license = "Apache-2.0"
+    author = "Contributors to the Eclipse Foundation <uprotocol-dev@eclipse.org>"
+    url = "https://github.com/eclipse-uprotocol/up-transport-zenoh-cpp"
+    description = "This library provides a Zenoh-based uProtocol transport for C++ uEntities"
+    topics = ("automotive", "iot", "uprotocol", "messaging")
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    test_requires = "gtest/1.14.0"
+
+    def requirements(self):
+        version_data = self.conan_data[self.version]
+        if "requirements" in version_data:
+            for requirement, version in version_data["requirements"].items():
+                self.requires(f"{requirement}/{version}")
+        else:
+            self.output.warning("No requirements specified in conandata.yml. Please check your configuration.")
+                
+
+    # We are providing our own cmake config since one is not included in the
+    # spec repo.
+    def export_sources(self):
+        copy(self, "CMakeLists.txt",
+             self.recipe_folder + "/..", self.export_sources_folder)
+
+    def source(self):
+        get(self, **self.conan_data[self.version]["sources"], strip_root=True)
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["up-transport-zenoh-cpp"]
